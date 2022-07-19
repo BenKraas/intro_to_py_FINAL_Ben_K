7bdf870d244cec0d3a1795078c8d76a80de2b1ee
KtRNofficial <64856011+KtRNofficial@users.noreply.github.com>
2022-07-19 21:24:46 +0200

    Working and spitting out means

--- Task_3_SURNAME.py
+++ Task_3_SURNAME.py
@@ -48,7 +48,9 @@ from os.path import exists
 
 def convert_to_lcz(*args, **kwargs) -> list:
     """
-    This functions calculates the LCZ (Local climate zone) given 7 parameters
+    This functions calculates the LCZ (Local climate zone) given 7 parameters.
+
+    Returns: (LCZ_code, LCZ_name)
 
     Accepted parameters are (in order):
     sky_view_f  : sky view factor
@@ -60,25 +62,44 @@ def convert_to_lcz(*args, **kwargs) -> list:
     terr_rough  : terrain roughness
 
     Alternatively, you can pass ALL these values as key-value pairs (kwargs)
-
     You must however EITHER pass all as args OR as kwargs. No mixing ;)
 
-    If a number is not given, the calculation will not take place 
-    and the funtion will raise a ValueError
-
-    Otherwise, the returned list will contain:
-    [LCZ_code, LCZ_name]
+    If a parameter is not given, the calculation can not take place 
 
     Author: Ben Kraas (https://github.com/KtRNofficial)
     """
 
     # nested functions
+    def import_lcz_data() -> object:
+        """
+        Private function specific to convert_to_lcz()
+
+        Imports/creates the lcz key data used to classify the lczs.
+        Returns the data as a pandas DataFrame.
+
+        The DataFrame is NOT zero-indexed! 
+        It is instead a 1-based index
+        This makes it much easier not to confuse the index with the LCZ codes!
+        """
+
+        if not exists('LCZ_key_data.json'):
+            print("JSON created!")
+            dump_data_json()
+        
+        with open('LCZ_key_data.json') as json_file:
+            data = json.load(json_file)
+
+        dataf = pd.DataFrame(data)
+        dataf.set_index("code", inplace=True)
+
+        return dataf
+
     def import_args(args: tuple, kwargs: dict) -> dict:
         """
+        Private function specific to convert_to_lcz()
+
         This function correctly imports all the args and kwargs for 
         convert_to_lcz()
-
-        Author: Ben Kraas (https://github.com/KtRNofficial)
         """
         config_dict = {}
         config_dict_n = ["sky_view_f", "aspect_r", "build_srf", "imperv_srf", \
@@ -96,78 +117,117 @@ def convert_to_lcz(*args, **kwargs) -> list:
         if args and isinstance(args, tuple) and (len(args) == len(config_dict_n)):
             for arg, name in zip(args, config_dict_n):
                 config_dict[name] = arg
-        
+
         # if kwargs are given
-        elif len(kwargs) == len(config_dict_n):
+        else:
             if all(name in kwargs.keys() for name in config_dict_n):
                 config_dict = kwargs
             else:
                 raise ValueError("convert_to_lcz(): \
                     Not all keys correspond to the required name! \
-                    Check your spelling of the kwargs ;) ")
+                    Check your spelling of kwargs ;) ")
 
-    # preparation
-    if not exists('LCZ_key_data.json'):
-        dump_data_json()
+        return config_dict
+
+    def dump_data_json():
+        """
+        Private function specific to convert_to_lcz()
+
+        Call this function to create the LCZ_key_data.json used in the function.
+        Automatically called by convert_to_lcz() if json does not exist
+        
+        Source: Task_3_LCZ_Thresholds.png
+        """
+
+        idx_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        nam_list = ["Compact high-rise", "Compact midrise", "Compact low-rise", \
+                    "Open high-rise", "Open midrise", "Open low-rise", \
+                    "Lightweight low-rise", "Large low-rise", "Sparsely built"]
+        svf_list = [[0.2, 0.4], [0.3, 0.6], [0.2, 0.6], [0.5, 0.7], \
+                    [0.5, 0.8], [0.6, 0.9], [0.2, 0.5], [0.7, 999], [0.8, 999]]
+        apr_list = [[2.00, 999], [0.75, 2.00], [0.75, 1.50],[0.75, 1.25], \
+                    [0.30, 0.75], [0.30, 0.75], [1.00, 2.00],[0.10, 0.30],\
+                    [0.10, 0.25]]
+        bsf_list = [[40, 60], [40, 70], [40, 70], [20, 40], \
+                    [20, 40], [20, 40], [60, 90], [30, 50], [10, 20]]
+        isf_list = [[40, 60], [30, 50], [10, 50], [30, 40], \
+                    [30, 50], [20, 50], [00, 20], [40, 50], [00, 20]]
+        psf_list = [[00, 10], [00, 20], [00, 30], [30, 40], \
+                    [20, 40], [30, 60], [00, 30], [00, 20], [60, 80]]
+        hre_list = [[25, 999], [10, 25], [3, 10], [25, 999],\
+                    [10, 25], [3, 10], [2, 4], [3, 10], [3, 10]]
+        trc_list = [[8, 8], [6, 7], [6, 6], [7, 8], \
+                    [5, 6], [5, 6], [4, 5], [5, 5], [5, 6]]
+        
+        LCZ_tresholds = {"code": idx_list,
+                        "name": nam_list,
+                        "sky_view_factor": svf_list,
+                        "aspect_ratio": apr_list,
+                        "building_surf_fract": bsf_list,
+                        "impervious_surf_fract":isf_list,
+                        "pervious_surf_fract":  psf_list,
+                        "height_of_rough_elem": hre_list,
+                        "terrain_rough_class":  trc_list
+                        }
+
+        with open("LCZ_key_data.json", "w") as fp:
+            json.dump(LCZ_tresholds , fp) # use indent=4 to make json more readable
+
+    def between(number: any, range_list: list) -> list:
+        """Checks if a number is between two values"""
+        if range_list[0] <= range_list[1]:
+            return range_list[0] <= number <= range_list[1]
+        else:
+            raise ValueError("First value in range_list must be smaller \
+                              than second value!")
+
+    def comb(keys, param_row):
+        """
+        Goes through the data and returns a DataFrames full of booleans.
+
+        A cell is set to 1 if it lies between the keys' required values
+
+        A cell is det to 0 if it doesn't.
+        """
+
+        param_row_list = list(param_row.keys())
+
+        # we need to add an arbitrary non-numeric value here ("x"). 
+        # since we don't know "name", we cannot check whether evaluates to true
+        # if we give "name" a string, it will skip this column in the for-loops
+        param_row_list.insert(0, "x")
+
+        finaldf = keys
+        for row in keys.index:
+            for col, key in zip(keys.columns, param_row_list):
+                if not key == "x":
+                    value = param_row[key]
+                    range = keys.loc[row,col]
+                    is_between = between(value, range)
+                    # if the value lies between the range values, it's set to 1
+                    if is_between:
+                        final_val = 1
+                    else:
+                        final_val = 0
+                    finaldf.loc[row,col] = final_val
+        return finaldf
+
+    # preparation of keys data 
+    # this table will be used to determine whether a number is within a range
+    LCZ_key = import_lcz_data()
+    numeric_cols = list(LCZ_key.columns)[1:]
     
-    with open('LCZ_key_data.json') as json_file:
-        data = json.load(json_file)
-
-    LCZ_data = pd.DataFrame(data)
-
-    # importing of kwargs
+    # importing of args/kwargs
+    # these are specifications which you want to check
     config_dict = import_args(args, kwargs)
-    print(config_dict)
 
     # function body
+    comb_bool = comb(LCZ_key, config_dict)
 
-
+    # only calculate the mean for all columns
     
-    return config_dict
 
-def dump_data_json():
-    """
-    Call this function to create the LCZ_key_data.json used in the function.
-    Automatically called by convert_to_lcz() if json does not exist
-    
-    Source: Task_3_LCZ_Thresholds.png
+    print(comb_bool[numeric_cols].mean(axis=1))
 
-    Author: Ben Kraas (https://github.com/KtRNofficial)
-    """
-
-    idx_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    nam_list = ["Compact high-rise", "Compact midrise", "Compact low-rise", \
-                "Open high-rise", "Open midrise", "Open low-rise", \
-                "Lightweight low-rise", "Large low-rise", "Sparsely built"]
-    svf_list = [[0.2, 0.4], [0.3, 0.6], [0.2, 0.6], [0.5, 0.7], \
-                [0.5, 0.8], [0.6, 0.9], [0.2, 0.5], [0.7, 999], [0.8, 999]]
-    apr_list = [[2.00, 999], [0.75, 2.00], [0.75, 1.50],[0.75, 1.25], \
-                [0.30, 0.75], [0.30, 0.75], [1.00, 2.00],[0.10, 0.30],\
-                [0.10, 0.25]]
-    bsf_list = [[40, 60], [40, 70], [40, 70], [20, 40], \
-                [20, 40], [20, 40], [60, 90], [30, 50], [10, 20]]
-    isf_list = [[40, 60], [30, 50], [10, 50], [30, 40], \
-                [30, 50], [20, 50], [00, 20], [40, 50], [00, 20]]
-    psf_list = [[00, 10], [00, 20], [00, 30], [30, 40], \
-                [20, 40], [30, 60], [00, 30], [00, 20], [60, 80]]
-    hre_list = [[25, 999], [10, 25], [3, 10], [25, 999],\
-                [10, 25], [3, 10], [2, 4], [3, 10], [3, 10]]
-    trc_list = [[8, 8], [6, 7], [6, 6], [7, 8], \
-                [5, 6], [5, 6], [4, 5], [5, 5], [5, 6]]
-    
-    LCZ_tresholds = {"code": idx_list,
-                     "name": nam_list,
-                     "sky_view_factor": svf_list,
-                     "aspect_ratio": apr_list,
-                     "building_surf_fract": bsf_list,
-                     "impervious_surf_fract":isf_list,
-                     "pervious_surf_fract":  psf_list,
-                     "height_of_rough_elem": hre_list,
-                     "terrain_rough_class":  trc_list
-                     }
-
-    with open("LCZ_key_data.json", "w") as fp:
-        json.dump(LCZ_tresholds , fp) # use indent=4 to make json more readable
-    
 if __name__ == "__main__":
-    convert_to_lcz(sky_view_f=1, aspect_r=1, build_srf=1, imperv_srf=1, perv_srf=1, hgt_rough=1, terr_rough=1)
+    convert_to_lcz(sky_view_f=0.4, aspect_r=0.90, build_srf=50, imperv_srf=40, perv_srf=25, hgt_rough=6, terr_rough=6)
