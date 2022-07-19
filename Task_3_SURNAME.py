9251f031c8d6fb09448436537af0b97606561818
KtRNofficial <64856011+KtRNofficial@users.noreply.github.com>
2022-07-19 19:07:23 +0200

    Implemented args and kwargs import

--- Task_3_SURNAME.py
+++ Task_3_SURNAME.py
@@ -46,7 +46,7 @@ import pandas as pd
 from os.path import exists
 
 
-def convert_to_lcz(**kwargs) -> list:
+def convert_to_lcz(*args, **kwargs) -> list:
     """
     This functions calculates the LCZ (Local climate zone) given 7 parameters
 
@@ -59,14 +59,52 @@ def convert_to_lcz(**kwargs) -> list:
     hgt_rough   : height of roughness elements
     terr_rough  : terrain roughness
 
+    Alternatively, you can pass ALL these values as key-value pairs (kwargs)
+
+    You must however EITHER pass all as args OR as kwargs. No mixing ;)
+
     If a number is not given, the calculation will not take place 
-    and the funtion will return a list of [False, False]
+    and the funtion will raise a ValueError
 
     Otherwise, the returned list will contain:
     [LCZ_code, LCZ_name]
 
     Author: Ben Kraas (https://github.com/KtRNofficial)
     """
+
+    # nested functions
+    def import_args(args: tuple, kwargs: dict) -> dict:
+        """
+        This function correctly imports all the args and kwargs for 
+        convert_to_lcz()
+
+        Author: Ben Kraas (https://github.com/KtRNofficial)
+        """
+        config_dict = {}
+        config_dict_n = ["sky_view_f", "aspect_r", "build_srf", "imperv_srf", \
+                            "perv_srf", "hgt_rough", "terr_rough"]
+
+        # check if the correct number of parameters are given
+        if len(args) != 7 and len(kwargs) != 7:
+            raise ValueError("convert_to_lcz(): \
+                Not the correct amout of numbers given")
+        elif len(args) > 0 and len(kwargs) > 0:
+            raise ValueError("convert_to_lcz(): \
+                You can only provide either args or kwargs!")
+        
+        # if args are given
+        if args and isinstance(args, tuple) and (len(args) == len(config_dict_n)):
+            for arg, name in zip(args, config_dict_n):
+                config_dict[name] = arg
+        
+        # if kwargs are given
+        elif len(kwargs) == len(config_dict_n):
+            if all(name in kwargs.keys() for name in config_dict_n):
+                config_dict = kwargs
+            else:
+                raise ValueError("convert_to_lcz(): \
+                    Not all keys correspond to the required name! \
+                    Check your spelling of the kwargs ;) ")
 
     # preparation
     if not exists('LCZ_key_data.json'):
@@ -78,15 +116,14 @@ def convert_to_lcz(**kwargs) -> list:
     LCZ_data = pd.DataFrame(data)
 
     # importing of kwargs
-    
-    config_dict = {}
-    if len(kwargs == 1) and type(kwargs) == "list":
-        print("it is a list")
-    else:
-        for content in kwargs:
-            config_dict[content] = content
+    config_dict = import_args(args, kwargs)
+    print(config_dict)
+
     # function body
 
+
+    
+    return config_dict
 
 def dump_data_json():
     """
@@ -129,8 +166,8 @@ def dump_data_json():
                      "terrain_rough_class":  trc_list
                      }
 
-    with open("LCZ_key_data", "w") as fp:
-        json.dump(LCZ_tresholds , fp) 
+    with open("LCZ_key_data.json", "w") as fp:
+        json.dump(LCZ_tresholds , fp) # use indent=4 to make json more readable
     
 if __name__ == "__main__":
-    convert_to_lcz([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    convert_to_lcz(sky_view_f=1, aspect_r=1, build_srf=1, imperv_srf=1, perv_srf=1, hgt_rough=1, terr_rough=1)
