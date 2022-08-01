7af245c90964e956f400c2e701b9af6aeae5e4f0
Ben <kastoron@gmail.com>
2022-08-01 22:28:53 +0200

    .

--- testscript.py
+++ testscript.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import bk_functions as bk
 import json
 
-pathobj = Path("lakes.geojson")
-jsonobj = bk.GeojsonObject()
-jsonobj.loadwd(pathobj)
-dicti = jsonobj.convert_to_multipoint()
+props = {"ttest": 1}
 
+var = props["one"]
+
+print(var)
