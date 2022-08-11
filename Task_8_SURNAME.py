bf6a282baa1d28e23bdf0b189ead89749a774d5c
Ben <kastoron@gmail.com>
2022-08-11 12:46:52 +0200

    Prepared formatting

--- Task_8_SURNAME.py
+++ Task_8_SURNAME.py
@@ -8,7 +8,11 @@ Final Project - TASK 8
 
 Use the air-quality-covid19-response data from Lecture 10
 (available on moodle) to address the following subtasks:
+"""
 
+
+
+"""
 Subtask 8.1
 -----------
 Create a new csv file that stores the 2015-2019 daily NO2 concetration
@@ -17,8 +21,11 @@ names. The result should be a single csv file.
 
 Hint: Use the Path class from the pathlib module to point to 
       the folder with air-quality-covid19-response data (see Lecture 10).
+"""
 
 
+
+"""
 Subtask 8.2
 -----------
 Calculate the monthly mean, SEM (standard error of the mean),
@@ -28,19 +35,23 @@ Answer the following:
 - Which month the NO2 becomes maximum and minimum in Cologne?
 - What is the NO2 inter-annual range (i.e., the difference between
   the max and min NO2 concentration) in each city?
+"""
 
 
+
+"""
 Subtask 8.3
 -----------
 Using the data from subtask 8.2 calculate the 95% lower and upper
 confidence intervals for each monhtly mean.
+"""
 
 
+
+"""
 Subtask 8.4
 -----------
 Use the csv from subtask 8.1 and calculate the monthly mean, minimum, and maximum
 NO2 per year and city. Create a figure with 3 lineplots (one subplot for each statistic)
 that present the corresponding monthly values for each german city.
-
 """
-
